c15c8370cbe28e4e791dc1b05e0ebe8efb10ef0b
Camille Coin <camillecoin.cc@gmail.com>
2020-08-16 11:43:59 +0200

    feat(Help): display all commands available

---
+++ commands/help.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const Discord = require('discord.js');
+
+module.exports.run = async(client, message, args, db) => {
+  fs.readdir("./commands/", (err, files) => {
+    if(err) console.error(err);
+
+    let jsfiles = files.filter(f => f.split(".").pop() === "js");
+    if(jsfiles.length <= 0) {
+        console.log("No commands to load!");
+        return;
+    }
+    let result = jsfiles.forEach((f, i) => {
+        let props = require(`./${f}`);
+        const embedMessage = new Discord.MessageEmbed()
+        .setColor()
+        .setTitle(`!${props.help.name}`)
+        .setDescription(props.help.description)
+
+        message.channel.send(embedMessage)
+    });
+});
+}
+
+module.exports.help = {
+  name: 'help',
+  description: "Renvoie une liste des commandes disponibles"
+}
