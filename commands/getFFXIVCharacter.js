60e068d429f509c621cf75ef294f78f5b35ec168
Camille Coin <camillecoin.cc@gmail.com>
2020-08-16 12:40:11 +0200

    reafactor(FFXIVChara): add a timeout for loading

--- commands/getFFXIVCharacter.js
+++ commands/getFFXIVCharacter.js
@@ -8,28 +8,44 @@ module.exports.run = (async function (client, message, args, db) {
   if (args.length < 3) {
     message.channel.send(`Il vous manque une information.`)
   } else {
-    axios.get(`https://xivapi.com/character/search?name=${args[0]}+${args[1]}&server=${args[2]}`)
-      .then(res => {
-        axios.get(`https://xivapi.com/character/${res.data.Results[0].ID}`)
-          .then(async res => {
-            const data = await res.data.Character
-            const id = data.ID.toString()
-            db.collection('users').doc(message.author.id).update({
-              'characters': [data.Name]
-            })
+    try {
+      const interval = setTimeout(() => {
+        message.channel.send('Veuillez patienter...')
+        .catch(err => {
+          console.log(error);
+          clearTimeout(interval)
+        })
+      }, 1000)
 
-            db.collection('characters').doc(id).set({
-              'id': data.ID,
-              'name': data.Name,
-              'server': data.Server,
-              'portrait': data.Portrait,
-              'freeCompanyId': data.FreeCompanyId,
-              'userId': message.author.id
-            }).then(() => {
-              message.channel.send(`✨ ${data.Name} a bien été enregistré.e pour ${message.author.username} !`)
+      let task = interval;
+
+
+      axios.get(`https://xivapi.com/character/search?name=${args[0]}+${args[1]}&server=${args[2]}`)
+        .then(res => {
+          axios.get(`https://xivapi.com/character/${res.data.Results[0].ID}`)
+            .then(async res => {
+              const data = await res.data.Character
+              const id = data.ID.toString()
+              db.collection('users').doc(message.author.id).update({
+                'characters': [data.Name]
+              })
+
+              db.collection('characters').doc(id).set({
+                'id': data.ID,
+                'name': data.Name,
+                'server': data.Server,
+                'portrait': data.Portrait,
+                'freeCompanyId': data.FreeCompanyId,
+                'userId': message.author.id
+              }).then(() => {
+                clearTimeout(task)
+                message.channel.send(`✨ ${data.Name} a bien été enregistré.e pour ${message.author.username} !`)
+              })
             })
-          })
-      })
+        })
+    } catch (error) {
+      if(error) message.channel.send(`🚫 Il y a eu une erreur pendant le processus.`)
+    }
   }
 })
 
