e270d0c169597dce331868b8fdb557e7f318a47b
Camille Coin <camillecoin.cc@gmail.com>
2020-08-16 10:34:38 +0200

    feat(FFXIV chara): create chara data in firebase

--- commands/getFFXIVCharacter.js
+++ commands/getFFXIVCharacter.js
@@ -3,20 +3,37 @@ const { MessageEmbed } = require("discord.js");
 
 const image = new MessageEmbed();
 
-module.exports.run = (client, message, args, db) => {
+module.exports.run = (async function (client, message, args, db) {
+
+  if (args.length < 3) {
+    message.channel.send(`Il vous manque une information.`)
+  } else {
     axios.get(`https://xivapi.com/character/search?name=${args[0]}+${args[1]}&server=${args[2]}`)
-    .then(res => {
-      const charaId = res.data.Results[0].ID;
-      axios.get(`https://xivapi.com/character/${charaId}`)
       .then(res => {
-        message.channel.send(`${res.data.Character.Name} :`, 
-        {files: [res.data.Character.Portrait]}
-        )
+        axios.get(`https://xivapi.com/character/${res.data.Results[0].ID}`)
+          .then(async res => {
+            const data = await res.data.Character
+            const id = data.ID.toString()
+            db.collection('users').doc(message.author.id).update({
+              'characters': [data.Name]
+            })
+
+            db.collection('characters').doc(id).set({
+              'id': data.ID,
+              'name': data.Name,
+              'server': data.Server,
+              'portrait': data.Portrait,
+              'freeCompanyId': data.FreeCompanyId,
+              'userId': message.author.id
+            }).then(() => {
+              message.channel.send(`✨ ${data.Name} a bien été enregistré.e pour ${message.author.username} !`)
+            })
+          })
       })
-    })
   }
+})
 
 module.exports.help = {
   name: 'chara',
-  description: "Renvoie les infos du personnage donné",
+  description: "Enregistre le personnage donné",
 }
